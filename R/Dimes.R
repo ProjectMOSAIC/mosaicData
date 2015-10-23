c23f45e0ac2fd9773036f7b2e91ca948793aa322
Randall Pruim <rpruim@gmail.com>
2015-10-23 08:51:17 -0400

    add Dimes data set -- need to decide if it stays.

---
+++ R/Dimes.R
@@ -0,0 +1,15 @@
+#' Weight of dimes
+#' 
+#' Weights of a sample of dimes
+#' 
+#' @docType data
+#' @name Dimes
+#' @usage data(Dimes)
+#' @format  A data frame with 30 observations on the following 2 variables.
+#' \itemize{
+#' \item{\code{mass}}{ mass of dime in grams}
+#' \item{\code{year}}{ year the dime was minted}
+#' }
+#' @source Data were collected by Michael Stob.
+NA
+
