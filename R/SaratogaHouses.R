efafa3753434734ea8a1d137c4be6a3060f4bc95
Randall Pruim <rpruim@gmail.com>
2015-11-29 20:10:01 -0500

    documentaion improvements

--- R/SaratogaHouses.R
+++ R/SaratogaHouses.R
@@ -7,22 +7,22 @@
 #' @usage data(SaratogaHouses)
 #' @format  A data frame with 1728 observations on the following 16 variables.
 #' \itemize{
-#' \item{\code{price}}{price (1000s of US dollars)}
-#' \item{\code{lotSize}}{size of lot (square feet)}
-#' \item{\code{age}}{age of house (years) }
-#' \item{\code{landValue}}{value of land (1000s of US dollars)}
-#' \item{\code{livingArea}}{living are (square feet) }
-#' \item{\code{pctCollege}}{percent of neighborhood that graduated college}
-#' \item{\code{bedrooms}}{number of bedrooms}
-#' \item{\code{firplaces}}{number of fireplaces}
-#' \item{\code{bathrooms}}{number of bathrooms (half bathrooms have no shower or tub)}
-#' \item{\code{rooms}}{number of rooms}
-#' \item{\code{heating}}{type of heating system}
-#' \item{\code{fuel}}{fuel used for heating}
-#' \item{\code{sewer}{type of sewer system}
-#' \item{\code{waterfront}}{whether property includes waterfront}
-#' \item{\code{newConstruction}}{whether the property is a new construction}
-#' \item{\code{centralAir}}{whether the house has central air}
+#' \item{\code{price}}{ price (1000s of US dollars)}
+#' \item{\code{lotSize}}{ size of lot (square feet)}
+#' \item{\code{age}}{ age of house (years)}
+#' \item{\code{landValue}}{ value of land (1000s of US dollars)}
+#' \item{\code{livingArea}}{ living are (square feet)}
+#' \item{\code{pctCollege}}{ percent of neighborhood that graduated college}
+#' \item{\code{bedrooms}}{ number of bedrooms}
+#' \item{\code{firplaces}}{ number of fireplaces}
+#' \item{\code{bathrooms}}{ number of bathrooms (half bathrooms have no shower or tub)}
+#' \item{\code{rooms}}{ number of rooms}
+#' \item{\code{heating}}{ type of heating system}
+#' \item{\code{fuel}}{ fuel used for heating}
+#' \item{\code{sewer}}{ type of sewer system}
+#' \item{\code{waterfront}}{ whether property includes waterfront}
+#' \item{\code{newConstruction}}{ whether the property is a new construction}
+#' \item{\code{centralAir}}{ whether the house has central air}
 #' }
 #' @source Data were originally collected by Candice Corvetti and Dick De Veaux 
 #' (Williams College) from \url{http://www.saratogacountyny.gov/departments/real-property-tax-service-agency/}.
