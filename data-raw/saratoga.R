0ad1176bae638a7677cf6b236ddcb28274f0acd0
Randall Pruim <rpruim@gmail.com>
2015-11-29 20:01:09 -0500

    adding SaratogaHouses data

---
+++ data-raw/saratoga.R
@@ -0,0 +1,53 @@
+require(mosaic)
+SaratogaHouses <- read.table("http://sites.williams.edu/rdeveaux/files/2014/09/Saratoga.txt", 
+  header=TRUE, stringsAsFactors=FALSE)
+inspect(SaratogaHouses)
+str(SaratogaHouses)
+SaratogaHouses <- 
+  SaratogaHouses %>%
+  mutate(
+    heating = derivedFactor('hot air' = Heat.Type == 2,
+                            'hot water/steam' = Heat.Type == 3,
+                            electric = Heat.Type == 4)) %>%
+  mutate(
+    fuel = derivedFactor(gas = Fuel.Type == 2,
+                             electric = Fuel.Type == 3,
+                             oil = Fuel.Type == 4)
+    ) %>%
+  mutate(
+    sewer = derivedFactor(private = Sewer.Type == 2,
+                              commercialprivate = Sewer.Type == 3,
+                              .default = NA)
+    ) %>%
+  mutate(
+    waterfront = derivedFactor(Yes = Waterfront, .default = "No")) %>%
+  mutate(
+    newConstruction = derivedFactor(Yes = New.Construct, .default = "No")) %>%
+  mutate( 
+    centralAir = derivedFactor(Yes = Central.Air, .default = "No")) %>%
+  rename( price = Price,
+          lotSize = Lot.Size,
+          age = Age,
+          landValue = Land.Value,
+          livingArea = Living.Area,
+          pctCollege = Pct.College,
+          bedrooms = Bedrooms,
+          fireplaces = Fireplaces,
+          bathrooms = Bathrooms,
+          rooms = Rooms)
+
+            
+  
+ 
+tally(heating ~ Heat.Type, data=SaratogaHouses)
+tally(fuel ~ Fuel.Type, data=SaratogaHouses)
+tally(sewer ~ Sewer.Type, data=SaratogaHouses)
+tally(centralAir ~ Central.Air, data=SaratogaHouses)
+SaratogaHouses <- 
+  SaratogaHouses %>% 
+  select(-Heat.Type, -Fuel.Type, -Sewer.Type, -Waterfront) %>%
+  select(- matches("\\."))
+devtools::use_data(SaratogaHouses, overwrite = TRUE) 
+# save(SaratogaHouses, file="SaratogaHouses.Rda")
+
+
