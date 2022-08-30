4ed6bc94e77c9a6c9c859c1ed906b810fc8ab633
Randall Pruim <rpruim@gmail.com>
2022-08-30 17:37:52 -0400

    recode dob in Marriage.  Closes #35.

--- data-raw/Marriage-recode.R
+++ data-raw/Marriage-recode.R
@@ -1,12 +1,36 @@
 
 # recoding some variables on 7/13/2020
 
-data(Marriage)  # from previous version of package
-Marriage <- 
-  Marriage %>% 
+# data(Marriage)  # from previous version of package
+# Marriage <-
+#   Marriage %>%
+#   mutate(
+#     appdate = lubridate::mdy(appdate),
+#     ceremonydate = lubridate::mdy(ceremonydate),
+#     dob = lubridate::mdy(dob),
+#     dayOfBirth = lubridate::yday(dob)
+#   )
+
+# fixing on 2022-08-30 because some dates were placed in the wrong century.
+
+Marriage <- readRDS('Marriage-orig.rds')
+Marriage <-
+  Marriage |>
   mutate(
     appdate = lubridate::mdy(appdate),
     ceremonydate = lubridate::mdy(ceremonydate),
     dob = lubridate::mdy(dob),
     dayOfBirth = lubridate::yday(dob)
   )
+Marriage <- Marriage |>
+  mutate(
+    dob = case_when(
+      dob > lubridate::ymd('1999-12-31') ~ dob - lubridate::years(100),
+      TRUE ~ dob)
+  )
+
+use_data(Marriage, overwrite = TRUE)
+
+library(mosaicData)
+range(Marriage$ceremonydate)
+range(Marriage$dob)
